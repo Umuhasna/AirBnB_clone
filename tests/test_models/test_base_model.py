c48d9564b58ed163e267f953eed435c1f1e31976
Umuhasna <umuhasna123@gmail.com>
2023-08-13 22:12:24 +0300

    Title: Fix
    Body: closes #5, closes #6
    
    Co-authored-by: Fuzzworth <nyangasi.m@gmail.com>

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -9,18 +9,6 @@ from uuid import UUID
 from datetime import datetime
 
 
-def mock_uuid():
-    '''
-    Docs
-    '''
-    return UUID("b6a6e15c-c67d-4312-9a75-9d084935e579")
-
-def mock_datetime():
-    '''
-    Docs
-    '''
-    return datetime(2017, 9, 28, 21, 5, 54, 119434)
-
 class TestBaseModel(unittest.TestCase):
     '''
     Docs
@@ -41,73 +29,75 @@ class TestBaseModel(unittest.TestCase):
     def test_methodDocsSave(self):
         '''
         '''
-        methodDoc = __import__("models.base_model").base_model.BaseModel.save.__doc__
+        methodDoc = (
+                __import__("models.base_model")
+                .base_model.BaseModel.save.__doc__)
         self.assertGreater(len(methodDoc), 0)
 
     def test_methodDocsto_dict(self):
         '''
         '''
-        methodDoc = __import__("models.base_model").base_model.BaseModel.to_dict.__doc__
+        methodDoc = (
+                __import__("models.base_model")
+                .base_model.BaseModel.to_dict.__doc__)
         self.assertGreater(len(methodDoc), 0)
 
     def test_methodDocs__str___(self):
         '''
         '''
-        methodDoc = __import__("models.base_model").base_model.BaseModel.__str__.__doc__
+        methodDoc = (
+                __import__("models.base_model")
+                .base_model.BaseModel.__str__.__doc__)
         self.assertGreater(len(methodDoc), 0)
 
     def test_idType(self):
         '''
         '''
         obj = BaseModel()
-        self.assertIs(obj.id, str)
+        self.assertIs(type(obj.id), str)
 
     def test_idLength(self):
         '''
         '''
         obj = BaseModel()
-        self.assertEqual(len(obj.id), 32)
+        self.assertEqual(len(obj.id), 36)
 
     def test_idValidity(self):
         '''
         '''
         obj = BaseModel()
         value = UUID(obj.id)
-        self.assertIs(value, UUID)
+        self.assertIs(type(value), UUID)
 
     def test_created_atType(self):
         '''
         '''
         obj = BaseModel()
-        self.assertIs(obj.created_at, datetime)
+        self.assertIs(type(obj.created_at), datetime)
 
     def test_updated_atType(self):
         '''
         '''
         obj = BaseModel()
-        self.assertIs(obj.updated_at, datetime.datetime)
+        self.assertIs(type(obj.updated_at), datetime)
 
-    @patch('datetime.now', mock_datetime)
-    @patch('uuid.uuid4', mock_uuid)
     def test_outputOf__str__(self):
         '''
         '''
         obj = BaseModel()
-        obj.name = "My First Model"
-        obj.my_number = 89
+        str1 = f"[BaseModel] ({obj.id}) {obj.__dict__}"
+        str2 = str(obj)
 
-        string1 = obj.__str__
-        string2 = "[BaseModel] (b6a6e15c-c67d-4312-9a75-9d084935e579) {'my_number': 89, 'name': 'My First Model', 'updated_at': datetime.datetime(2017, 9, 28, 21, 5, 54, 119434), 'id': 'b6a6e15c-c67d-4312-9a75-9d084935e579', 'created_at': datetime.datetime(2017, 9, 28, 21, 5, 54, 119434)}"
-
-        self.assertEqual(string1, string2)
+        self.assertEqual(str1, str2)
 
     def test_updated_atChanged(self):
         '''
         '''
         obj = BaseModel()
-        time1 = obj.updated_at
-        obj.id = uuid.uuid4()
-        self.assertGreater(obj.updated_at, time1)
+        initial_value = obj.updated_at
+        obj.save()
+
+        self.assertGreater(obj.updated_at, initial_value)
 
     def test_to_dictCheck(self):
         '''
@@ -118,7 +108,6 @@ class TestBaseModel(unittest.TestCase):
 
         for keys in __dict__dict:
             self.assertIn(keys, to_dict_dict)
-            self.assertEqual(__dict__dict[keys], to_dict_dict[keys])
 
     def test_to_dict(self):
         '''
@@ -127,22 +116,22 @@ class TestBaseModel(unittest.TestCase):
         to_dict_dict = obj.to_dict()
 
         self.assertIn("__class__", to_dict_dict)
-        self.assertIs(to_dict_dict["__class__"], str)
+        self.assertIs(type(to_dict_dict["__class__"]), str)
 
-    @patch('datetime.now', mock_datetime)
     def test_to_dict_Valid(self):
         '''
         '''
-        value = "2017-09-28T21:05:54.119434"
         obj = BaseModel()
         to_dict_dict = obj.to_dict()
+        created_at = datetime.fromisoformat(to_dict_dict["created_at"])
+        updated_at = datetime.fromisoformat(to_dict_dict["updated_at"])
 
         self.assertIn("created_at", to_dict_dict)
         self.assertIn("updated_at", to_dict_dict)
-        self.assertIs(to_dict_dict["created_at"], str)
-        self.assertIs(to_dict_dict["updated_at"], str)
-        self.assertEqual(to_dict_dict["created_at"], value)
-        self.assertEqual(to_dict_dict["updated_at"], value)
+        self.assertIs(type(to_dict_dict["created_at"]), str)
+        self.assertIs(type(to_dict_dict["updated_at"]), str)
+        self.assertEqual(to_dict_dict["created_at"], created_at.isoformat())
+        self.assertEqual(to_dict_dict["updated_at"], updated_at.isoformat())
 
     def test_dictType(self):
         '''
@@ -150,4 +139,4 @@ class TestBaseModel(unittest.TestCase):
         obj = BaseModel()
         to_dict_dict = obj.to_dict()
 
-        self.assertIs(to_dict_dict["id"], str)
+        self.assertIs(type(to_dict_dict["id"]), str)
