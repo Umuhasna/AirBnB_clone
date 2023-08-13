8961a536607576fbf35666cbc96dd4662eab866a
Umuhasna <umuhasna123@gmail.com>
2023-08-14 02:03:29 +0300

    Test

--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,7 @@ Module Docs:
 '''
 from datetime import datetime
 import uuid
+from models import storage
 
 
 class BaseModel:
@@ -29,12 +30,14 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+            storage.new(self)
 
     def save(self):
         '''
         Docs
         '''
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         '''
---
+++ models/engine/file_storage.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+'''
+Module Docs
+'''
+from json import dumps, loads
+from os.path import isfile
+
+
+class FileStorage:
+    '''
+    Class Docs
+    '''
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        '''
+        Docs
+        '''
+        return FileStorage.__objects
+
+    def new(self, obj):
+        '''
+        Docs
+        '''
+        key = f"{obj.__class__.__name__}.{obj.id}"
+
+        FileStorage.__objects[key] = obj
+
+    def save(self):
+        '''
+        Docs
+        '''
+        final_dict = {key: value.to_dict() for key, value in FileStorage.__objects.items()}
+        json_string = dumps(final_dict)
+        filename = FileStorage.__file_path
+        with open(filename, "w") as f:
+            f.write(json_string)
+
+    def reload(self):
+        '''
+        Docs
+        '''
+        filename = FileStorage.__file_path
+        if isfile(filename):
+            with open(filename, "r") as f:
+                line = f.readline()
+                final_dict = loads(line)
+            for key, value in final_dict.items():
+                if value["__class__"] == "BaseModel":
+                    dummy_obj = self.__class__(value)
+                else:
+                    dummy_obj = self.__class__()
+
+                self.new(dummy_obj)
