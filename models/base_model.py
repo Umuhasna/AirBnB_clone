b1b7ab539ac98c7cdeb78f9a88091e9bdc4e7e0c
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-08-13 22:37:39 +0200

    Title: Task 4
    Body: closes #13 closes #14
    
    Co-authored-by: Umuhasna <umuhasna123@gmail.com>

--- models/base_model.py
+++ models/base_model.py
@@ -3,6 +3,7 @@
 Module Docs:
 '''
 from datetime import datetime
+import dateutil.parser
 import uuid
 
 
@@ -11,13 +12,24 @@ class BaseModel:
     Class Docs
     '''
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         '''
         Docs
         '''
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at':
+                    value = dateutil.parser.isoparse(value)
+                    self.created_at = value;
+                elif key == 'updated_at':
+                    value = dateutil.parser.isoparse(value)
+                    self.updated_at = value;
+                elif key == 'id':
+                    self.id = str(value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def save(self):
         '''
