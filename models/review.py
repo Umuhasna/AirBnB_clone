f4d5228a9dd9c569e0d0d258556032169bddf480
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-08-14 22:06:17 +0200

    Title: Task 8, 9 and 10
    Co-authored-by: Umuhasna <umuhasna123@gmail.com>

---
+++ models/review.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''
+Module Docs:
+'''
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    '''
+    Class Docs
+    '''
+
+    place_id = ""
+    user_id = ""
+    text = ""
---
+++ models/state.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''
+Module Docs:
+'''
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    '''
+    Class Docs
+    '''
+
+    name = ""
